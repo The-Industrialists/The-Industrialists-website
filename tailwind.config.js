717c2a08ce91d6930ee360927881356aa9f978c1
editspace <editspace@protonmail.com>
2024-06-23 14:56:30 -0700

    fixed tailwind css not wanting to work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,css}", "./index.html"],
+  content: ["./src/**/*.js", "./index.html"],
   theme: {
     extend: {},
   },
