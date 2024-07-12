2357b611ed490513c9fd74adad2448e5659e6381
editspace <editspace@protonmail.com>
2024-07-12 09:59:07 -0700

    fixed about page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
-import { FastForward } from 'lucide-react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }),]
+  base: "/The-Industrialists-website/", // Ensure this is correct
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
