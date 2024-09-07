f55e65ac390bac0d2d88e70660427f44996e2afb
editspace <editspace@protonmail.com>
2024-09-06 17:40:15 -0700

    updated astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://the-industrialists.github.io/The-Industrialists-website",
-  base: "/The-Industrialists-website/", // Ensure this is correct
+  base: "/The-Industrialists-website/",
   integrations: [
     react(),
     tailwind({
